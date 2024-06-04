828571db51d7a71ca18128d97d1dd8697a94e91b
Daniil Basiuc-Brinzei <bioxdevcompany@gmail.com>
2024-06-04 21:17:32 +0300

    Update ChangeAnswers.php

--- app/Console/Commands/ChangeAnswers.php
+++ app/Console/Commands/ChangeAnswers.php
@@ -4,16 +4,17 @@ namespace App\Console\Commands;
 
 use App\Models\Project;
 use App\Models\Question;
+use App\Models\Response;
 use Illuminate\Console\Command;
 
-class RandomizeResponses extends Command
+class ChangeAnswers extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'app:randomize';
+    protected $signature = 'app:answer {answer?}';
 
     /**
      * The console command description.
@@ -25,18 +26,32 @@ class RandomizeResponses extends Command
     /**
      * Execute the console command.
      */
-    public function handle()
+    public function handle(): void
     {
         $questions = Question::all();
         $projects = Project::all();
-        $display = ['', 'Yes', 'No', 'Unrelated'];
+        $display = collect([
+            Response::YES => 'Yes',
+            Response::NO => 'No',
+            Response::UNRELATED => 'Unrelated',
+        ]);
+
+
+        $forceAnswer = $this->argument("answer");
+        if (isset($forceAnswer)) {
+            if (!$display->has($forceAnswer)) {
+                $this->error("Invalid answer type");
+
+                return;
+            }
+        }
 
         foreach ($projects as $project) {
             $this->info("********************************************************");
             $this->info("Project: ".$project->title);
             $this->info("********************************************************");
             foreach ($questions as $question) {
-                $answer = rand(1, 3);
+                $answer = $forceAnswer ?? rand(1, 3);
 
                 $this->warn(sprintf("Answer: %10s : %s", $display[$answer], $question->question));
                 $project->setAnswerFor($question, $answer);
