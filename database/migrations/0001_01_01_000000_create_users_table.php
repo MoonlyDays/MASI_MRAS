696e7493caa034d83dbf5b1f38fa116c61c8d48c
Daniel Basiuc-Brinzei <32462579+MoonlyDays@users.noreply.github.com>
2024-11-17 15:46:49 +0200

    Update 0001_01_01_000000_create_users_table.php

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('password')->nullable();
-            $table->unsignedInteger('discord_id')->nullable()->unique();
+            $table->string('discord_id')->nullable()->unique();
             $table->rememberToken();
             $table->timestamps();
         });
